84e83e92229ce0db974ebf595356ea847e8ec640
Miti Mazmudar <mitimazmudar@gmail.com>
2023-05-30 01:05:14 -0400

    feat: add a refactored normalization algo, tests for helper functions

--- routing/src/lib.rs
+++ routing/src/lib.rs
@@ -1,3 +1,7 @@
+use rand::distributions::{Distribution, Uniform};
+use rand_xoshiro::SplitMix64;
+use std::borrow::{Borrow, BorrowMut};
+
 const K: usize = 4;
 const NO_OF_BUCKETS: usize = 4;
 const OWN_NODE_ID: &'static str = "0011";
@@ -56,6 +60,75 @@ impl RoutingTable {
         self
     }
 
+    fn get_records_from_previous_buckets(
+        rand: &mut SplitMix64,
+        n: usize,
+        previous_buckets: &[Bucket],
+    ) -> Vec<Record> {
+        if n == 0 {
+            return vec![];
+        }
+
+        let prev_records: Vec<Record> = previous_buckets
+            .iter()
+            .flat_map(|bucket| bucket.records.clone())
+            // .take(n)
+            .collect();
+
+        if prev_records.len() <= n {
+            return prev_records;
+        }
+
+        Uniform::from(0..prev_records.len())
+            .sample_iter(rand)
+            .take(n)
+            .map(|i| prev_records[i].clone())
+            .collect()
+    }
+
+    fn get_records_from_next_buckets(
+        rand: &mut SplitMix64,
+        n: usize,
+        next_buckets: &[Bucket],
+    ) -> Vec<Record> {
+        vec![]
+    }
+
+    pub fn normalize2(self, rand: &mut SplitMix64) -> RoutingTable {
+        let normalized_buckets: Vec<Bucket> = self
+            .buckets
+            .iter()
+            .enumerate()
+            .map(|(index, bucket)| {
+                let normalized_records_size = bucket.records.len();
+
+                let records_from_prev_buckets = RoutingTable::get_records_from_previous_buckets(
+                    rand,
+                    K - normalized_records_size,
+                    &self.buckets[0..index],
+                );
+
+                let records_from_next_buckets = RoutingTable::get_records_from_next_buckets(
+                    rand,
+                    K - normalized_records_size - records_from_prev_buckets.len(),
+                    &self.buckets[index..self.buckets.len()],
+                );
+
+                let mut normalized_records = bucket.records.clone();
+                normalized_records.extend(records_from_prev_buckets);
+                normalized_records.extend(records_from_next_buckets);
+
+                Bucket {
+                    records: normalized_records,
+                }
+            })
+            .collect();
+
+        RoutingTable {
+            buckets: normalized_buckets,
+        }
+    }
+
     pub fn normalize(self) -> RoutingTable {
         let buckets = self.buckets;
 
@@ -102,5 +175,32 @@ impl RoutingTable {
         RoutingTable {
             buckets: normalized_buckets,
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use rand_core::SeedableRng;
+    use std::borrow::BorrowMut;
+
+    #[test]
+    fn test_empty_prev_buckets() {
+        assert!(RoutingTable::get_records_from_previous_buckets(
+            SplitMix64::seed_from_u64(0).borrow_mut(),
+            0,
+            &[Bucket::new()]
+        )
+        .is_empty());
+    }
+
+    #[test]
+    fn test_empty_next_buckets() {
+        assert!(RoutingTable::get_records_from_next_buckets(
+            SplitMix64::seed_from_u64(0).borrow_mut(),
+            0,
+            &[Bucket::new()]
+        )
+        .is_empty());
     }
 }
