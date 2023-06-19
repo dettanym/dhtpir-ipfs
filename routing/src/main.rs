d339afff24d972b013e4aa7905ce9c60eb4aa76b
Miti Mazmudar <mitimazmudar@gmail.com>
2023-06-19 14:34:46 -0400

    refactor: fix call from main to new normalize fn

--- routing/src/main.rs
+++ routing/src/main.rs
@@ -1,10 +1,13 @@
 use crate::lib::RoutingTable;
+use rand_core::SeedableRng;
+use rand_xoshiro::SplitMix64;
+use std::borrow::BorrowMut;
 
 mod lib;
 
 fn main() {
     let new_rt = RoutingTable::new()
         .add_record(0, "0010", "RE 0010")
-        .normalize();
+        .normalize(SplitMix64::seed_from_u64(0).borrow_mut());
     dbg!("{:?}", new_rt);
 }
