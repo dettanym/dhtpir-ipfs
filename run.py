e27a6b063e1a5eb5c3292625f7384a26d30d15d2
Rasoul Akhavan Mahdavi <rasoul.akhavan.mahdavi@uwaterloo.ca>
2024-04-26 23:40:04 +0000

    Changed how spiral is tested

--- run.py
+++ run.py
@@ -96,42 +96,50 @@ def parse_sealpir(log_dir, num_rows, item_size):
         json.dump(data_dict, json_file, indent=4)
 
 def parse_spiral(log_dir, num_rows, item_size, version="spiral"):
-    
-    with open(f"{log_dir}/{version}.txt", 'r') as file:
-        log_content = file.read()
-    
-    patterns = {
-        "Number of items" : r"Number of items\s*:\s*(\d+)",
-        "n" : r"PIR over n=(\d+) elements",
-        "Item size (B)" : r"Item size\s*:\s*(\d+)",
-        "size (B)" : r"elements of size (\d+) bytes each",
-        "Total offline query size (B)": r"Total offline query size\s*\(b\)\s*:\s*(\d+)",
-        "First dimension": r"First dimension\s*\(b\)\s*:\s*(\d+)",
-        "Total for other dimensions": r"Total for other dimensions\s*\(b\)\s*:\s*(\d+)",
-        "Total online query size (B)": r"Total online query size\s*\(b\)\s*:\s*(\d+)",
-        "Response size (B)": r"Response size\s*\(b\)\s*:\s*(\d+)",
-        "Main expansion (us)": r"Main expansion\s*\(CPU·us\)\s*:\s*(\d+)",
-        "Further dimension expansion (us)": r"Further dimension expansion\s*\(CPU·us\)\s*:\s*(\d+)",
-        "Conversion (us)": r"Conversion \(CPU·us\)\s*:\s*(\d+)",
-        "Database-independent computation Total (us)": r"Database-independent computation.*?Total \(CPU·us\)\s*:\s*(\d+)",
-        "First dimension multiply (us)": r"First dimension multiply \(CPU·us\)\s*:\s*(\d+)",
-        "Folding (us)": r"Folding \(CPU·us\): (\d+)",
-        "Database-dependent computation Total (us)": r"Database-dependent computation.*?Total \(CPU·us\)\s*:\s*(\d+)",
-        "Key generation (us)": r"Key generation \(CPU·us\)\s*:\s*(\d+)",
-        "Query generation (us)": r"Query generation \(CPU·us\)\s*:\s*(\d+)",
-        "Decoding (us)": r"Decoding \(CPU·us\)\s*:\s*(\d+)"
-    }
-    
-    results = {}
-    for field_name, pattern in patterns.items():
-        match = re.search(pattern, log_content, re.DOTALL)
-        if match:
-            results[field_name] = int(match.group(1))
-        else:
-            results[field_name] = None
-    
+
+    log_content = json.load(open(f"{log_dir}/{version}.txt", 'r'))
+
+    log_content["Number of items"] = num_rows
+    log_content["Item size (B)"] = item_size
+
     with open(f'{log_dir}/{version}.json', 'w') as json_file:
-        json.dump(results, json_file, indent=4)
+        json.dump(log_content, json_file, indent=4)
+
+    # with open(f"{log_dir}/{version}.txt", 'r') as file:
+    #     log_content = file.read()
+    
+    # patterns = {
+    #     "Number of items" : r"Number of items\s*:\s*(\d+)",
+    #     "n" : r"PIR over n=(\d+) elements",
+    #     "Item size (B)" : r"Item size\s*:\s*(\d+)",
+    #     "size (B)" : r"elements of size (\d+) bytes each",
+    #     "Total offline query size (B)": r"Total offline query size\s*\(b\)\s*:\s*(\d+)",
+    #     "First dimension": r"First dimension\s*\(b\)\s*:\s*(\d+)",
+    #     "Total for other dimensions": r"Total for other dimensions\s*\(b\)\s*:\s*(\d+)",
+    #     "Total online query size (B)": r"Total online query size\s*\(b\)\s*:\s*(\d+)",
+    #     "Response size (B)": r"Response size\s*\(b\)\s*:\s*(\d+)",
+    #     "Main expansion (us)": r"Main expansion\s*\(CPU·us\)\s*:\s*(\d+)",
+    #     "Further dimension expansion (us)": r"Further dimension expansion\s*\(CPU·us\)\s*:\s*(\d+)",
+    #     "Conversion (us)": r"Conversion \(CPU·us\)\s*:\s*(\d+)",
+    #     "Database-independent computation Total (us)": r"Database-independent computation.*?Total \(CPU·us\)\s*:\s*(\d+)",
+    #     "First dimension multiply (us)": r"First dimension multiply \(CPU·us\)\s*:\s*(\d+)",
+    #     "Folding (us)": r"Folding \(CPU·us\): (\d+)",
+    #     "Database-dependent computation Total (us)": r"Database-dependent computation.*?Total \(CPU·us\)\s*:\s*(\d+)",
+    #     "Key generation (us)": r"Key generation \(CPU·us\)\s*:\s*(\d+)",
+    #     "Query generation (us)": r"Query generation \(CPU·us\)\s*:\s*(\d+)",
+    #     "Decoding (us)": r"Decoding \(CPU·us\)\s*:\s*(\d+)"
+    # }
+    
+    # results = {}
+    # for field_name, pattern in patterns.items():
+    #     match = re.search(pattern, log_content, re.DOTALL)
+    #     if match:
+    #         results[field_name] = int(match.group(1))
+    #     else:
+    #         results[field_name] = None
+    
+    # with open(f'{log_dir}/{version}.json', 'w') as json_file:
+    #     json.dump(results, json_file, indent=4)
               
 def parse_fastpir(log_dir, num_rows, item_size):
     # Open the file and read the content
@@ -239,7 +247,7 @@ def main():
     
     # Payload size of 0 means that each protocol will the largest payload size it supports
     for payload_byte_size in [0]:
-        for log_num_rows in [11, 12]:
+        for log_num_rows in [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]:
             num_rows = 2**log_num_rows
             log_dir = f"logs/logs-{num_rows}-{payload_byte_size}"
             
@@ -277,53 +285,41 @@ def main():
                 print("Failed to run OnionPIR")
 
             # Spiral Variants
-            first_dim = 8
-            other_dim = log_num_rows - first_dim
-            base_spiral_command = ["spiral-clone/build/spiral", str(first_dim), str(other_dim), "0"]
-
-            # Spiral
-            command = base_spiral_command
-            if run_command(command, f"{log_dir}/spiral.txt"):
-                parse_spiral(log_dir, num_rows, payload_byte_size, 'spiral')
-            else:
-                print("Failed to run Spiral")
-
-            # SpiralStream
-            command = base_spiral_command + ["--direct-upload"]
-            if run_command(command, f"{log_dir}/spiral-stream.txt"):
-                parse_spiral(log_dir, num_rows, payload_byte_size, 'spiral-stream')
-            else:
-                print("Failed to run SpiralStream")
-
-            # SpiralPack
-            command = base_spiral_command + ["--high-rate"]
-            if run_command(command, f"{log_dir}/spiral-pack.txt"):
-                parse_spiral(log_dir, num_rows, payload_byte_size, 'spiral-pack')
-            else:
-                print("Failed to run SpiralPack")
-
-            # SpiralStreamPack
-            command = base_spiral_command + ["--direct-upload", "--high-rate"]
-            if run_command(command, f"{log_dir}/spiral-stream-pack.txt"):
-                parse_spiral(log_dir, num_rows, payload_byte_size, 'spiral-stream-pack')
-            else:
-                print("Failed to run SpiralStreamPack")
             
-            # RLWEPIR
-            rlwe_payload_byte_size = 256*1024 if payload_byte_size == 0 else payload_byte_size
-            log2_num_rows = math.ceil(math.log2(num_rows))
-            for mode in ["RLWE_All_Keys", "RLWE_Whispir_3_Keys", "RLWE_Whispir_2_Keys"]:
-                command = [f"go", "test", "-v", "-timeout", "99999s", "./pir/...", "-run", "E2E"]
-                env_vars = {
-                    'LOG2_NUMBER_OF_ROWS': f'{log2_num_rows}',
-                    'LOG2_NUM_DB_ROWS': f'{log2_num_rows}',
-                    'ROW_SIZE': f'{rlwe_payload_byte_size}',
-                    'MODE': f'{mode}'
-                }
-                if run_command(command, f"{log_dir}/{mode}.txt", cwd="private-zikade", env=env_vars):
-                    parse_rlwepir(log_dir, num_rows, rlwe_payload_byte_size, mode)
+            # Hacky line. Should fix it later
+            run_command_no_output(["cp", "spiral-clone/build/spiral", "spiral-clone/"])
+
+            spiral_payload_byte_size = 256*1024 if payload_byte_size == 0 else payload_byte_size
+            base_spiral_command = ["python3", "select_params.py", "--quiet", "--skip-cmake", "--skip-make", f"{log_num_rows}", f"{spiral_payload_byte_size}"]
+            for flags, name in [
+                ([], "spiral"),
+                (["--direct-upload"], "spiral-stream"),
+                (["--pack"], "spiral-pack"),
+                (["--direct-upload", "--pack"], "spiral-stream-pack")
+            ]:
+                command = base_spiral_command
+                if run_command(command+flags, f"{log_dir}/{name}.txt", cwd="spiral-clone"):
+                    parse_spiral(log_dir, num_rows, spiral_payload_byte_size, name)
                 else:
-                    print(f"Failed to run {mode}")
+                    print("Failed to run Spiral")
+
+            # RLWEPIR
+            if log2_num_rows <= 16:
+                rlwe_payload_byte_size = 256*1024 if payload_byte_size == 0 else payload_byte_size
+                log2_num_rows = math.ceil(math.log2(num_rows))
+                # for mode in ["RLWE_All_Keys", "RLWE_Whispir_3_Keys", "RLWE_Whispir_2_Keys"]:
+                for mode in ["RLWE_All_Keys"]:
+                    command = [f"go", "test", "-v", "-timeout", "99999s", "./pir/...", "-run", "E2E"]
+                    env_vars = {
+                        'LOG2_NUMBER_OF_ROWS': f'{log2_num_rows}',
+                        'LOG2_NUM_DB_ROWS': f'{log2_num_rows}',
+                        'ROW_SIZE': f'{rlwe_payload_byte_size}',
+                        'MODE': f'{mode}'
+                    }
+                    if run_command(command, f"{log_dir}/{mode}.txt", cwd="private-zikade", env=env_vars):
+                        parse_rlwepir(log_dir, num_rows, rlwe_payload_byte_size, mode)
+                    else:
+                        print(f"Failed to run {mode}")
 
             print("Done")
 
